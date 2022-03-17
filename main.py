a99f5fe02877e1a685aaeb20dff3498f794ef28e
almirkalimulin <almiruwow@gmail.com>
2022-03-17 23:42:31 +0300

    improved the program with the help of classes

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
 from flask import Flask
+from classes.return_result import Result
 import json
 
-with open("candidates.json", "r") as f:
-    text_json = json.load(f)
+
+def json_d():
+    with open("candidates.json", "r") as f:
+        text_json = json.load(f)
+    return text_json
+
 
 app = Flask(__name__)
+json = Result(json_d())
 
 
 @app.route("/")
 def page_index():
-    str_ = ''
-    for dict_ in text_json:
-        str_ += dict_['name'] + '\n' + str(dict_['id']) + '\n' + dict_['skills'] + '\n' + '\n'
-    return f"<pre>{str_}</pre>"
+    return json.home_page()
 
 
 @app.route("/candidate/<can>/")
 def page_profile(can):
-    return_candidate = text_json[int(can) - 1]
-    return f'<img src={return_candidate["picture"]}>' + f'<pre>{return_candidate["name"]}\n{return_candidate["id"]}\n{return_candidate["skills"]}</pre>'
+    return json.candidate(can)
 
 
 @app.route("/skill/<cand_skills>/")
 def page_feed(cand_skills):
-    str_ = ''
-    for dict_ in text_json:
-        new_dict = dict_["skills"].lower().split(', ')
-        if cand_skills in new_dict:
-            str_ += dict_['name'] + '\n' + str(dict_['id']) + '\n' + dict_['skills'] + '\n' + '\n'
-    return f"<pre>{str_}</pre>"
+    return json.skills(cand_skills)
 
-app.run(host='0.0.0.0', port=8000)
+
+if __name__ == "__main__":
+    app.run()
+
+
+
