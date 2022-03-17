05fea7db5952106c32eeb06051074305b22dded3
almirkalimulin <almiruwow@gmail.com>
2022-03-17 23:57:28 +0300

    add encoding utf-8

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import json
 
 
 def json_d():
-    with open("candidates.json", "r") as f:
+    with open("candidates.json", "r", encoding="utf-8") as f:
         text_json = json.load(f)
     return text_json
 
